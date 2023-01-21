7425d32514aece6551c258b5cbcf872a0ad04a43
ajin3830 <ajin3830@gmail.com>
2023-01-21 12:24:41 -0500

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,45 +1,66 @@
 // write your code here
-// See the image including its title, likes and comments 
-
-function fetchCard () {
-    fetch('http://localhost:3000/images/1')
-    .then(response => response.json())
-    .then(data => renderCard(data))
-}
-
-document.querySelector("#card-title").textContent = 'Woofing those bugs away'
-document.querySelector("#card-image").src = './assets/coder-dog.png'
-
-function renderCard(images) {
-    images.forEach( image=> {
-        const content = document.querySelector("#comments-list")
-        const li = document.createElement('li')
-        li.textContent = comments.content
-        // FIX IT !
-    })
-}
-const button = document.querySelector("#like-button")
-
-button.addEventListener("click", (e) => {
-    e.preventDefault()
-    document.querySelector("#like-count.likes").textContent += 1 
-    // string + number!
-})
-
-const form = document.querySelector("#comment-form")
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    let newComment = { comment: e.target['newComment'].value}
-    renderCard([newComment])
-})
-
-renderCard()
-
-fetchCard()
+// 1. See the image including its title, likes and comments 
 
 // 2. Click on the heart icon to increase image likes on the page. 
 // No persistence is needed.
 
 // 3. Add a new comment to the page when the comment form is submitted. 
 // No persistence is needed.
+
+function fetchImage () {
+    fetch('http://localhost:3000/images/1')
+    .then(response => response.json())
+    .then(data => renderImage(data))
+}
+
+function renderImage (image) {
+// title
+    document.querySelector("#card-title").textContent = image.title
+// image
+    document.querySelector("#card-image").src = image.image
+}
+
+fetchImage()
+
+function fetchComment () {
+    fetch('http://localhost:3000/comments')
+    .then(response => response.json())
+    .then(data => renderComment(data))
+}
+
+const commentsList = document.querySelector("#comments-list")
+function renderComment(comments) {
+    comments.forEach(comment => {
+        const li = document.querySelector("li")
+        li.textContent = comment.content
+        commentsList.appendChild(li)
+        // console.log(comment)
+    })
+}
+
+fetchComment ()
+
+const button = document.querySelector("#like-button")
+const likeCount = document.querySelector("#like-count")
+let numLikes = 0
+
+button.addEventListener("click", (e) => {
+    e.preventDefault()
+    numLikes += 1
+    likeCount.textContent = `${numLikes} likes`
+    console.log(likeCount)
+    
+})
+
+const form = document.querySelector("#comment-form")
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let newComment = e.target['comment'].value
+    const li = document.createElement("li")
+    li.textContent = newComment
+    commentsList.appendChild(li)
+    console.log(newComment)
+})
+
+
+
